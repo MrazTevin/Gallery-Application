f16ff4c7b21e8a2fd923d517493b5a1700f2acd2
MrazTevin <millatevin@gmail.com>
2018-02-24 12:09:00 +0300

    comment for image model

--- exhibit/display/models.py
+++ exhibit/display/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 # Create your models here.
 
+# Image model to display image details
+
 
 class Images(models.Model):
     image_name = models.CharField(max_length=20)
