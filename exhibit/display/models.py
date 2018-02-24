48cee98f30a6fb2ae7ed94e9c105c8cc842fc526
MrazTevin <millatevin@gmail.com>
2018-02-24 12:06:39 +0300

    image name and image description field

--- exhibit/display/models.py
+++ exhibit/display/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Images(models.Model):
+    image_name = models.CharField(max_length=20)
+    image_description = models.CharField(max_length=30)
