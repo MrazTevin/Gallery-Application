a05ed1b50d901230e0671ea87ee51fc775c9f895
MrazTevin <millatevin@gmail.com>
2018-02-26 02:57:40 +0300

    model for my image location

--- exhibit/display/models.py
+++ exhibit/display/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+# Location model to enable us choose location
+
+
+class Location(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
