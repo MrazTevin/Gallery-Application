c643528e7ea84edb64f43d601ac2bb82451533aa
MrazTevin <millatevin@gmail.com>
2018-02-25 21:23:02 +0300

    Put the location and category models before the image model

--- exhibit/display/models.py
+++ exhibit/display/models.py
@@ -2,31 +2,8 @@ from django.db import models
 
 # Create your models here.
 
-# Image model to display image details
-
-
-class Images(models.Model):
-    image_name = models.CharField(max_length=20)
-    image_description = models.CharField(max_length=30)
-    # post will contain the image content
-    post = models.TextField()
-    # one location may describe many images while many images may have one
-    # location
-    location = models.ForeignKey(Location)
-    category = models.ForeignKey(Category)
-
-    # __str__ will return string representation of the image model
-    # __str__ will enable us view our returned queries
-
-    def __str__(self):
-        return self.image_name
-
-    class Meta:
-        ordering = ['name']
 
 # Location model to enable us choose location
-
-
 class Location(models.Model):
     name = models.CharField(max_length=30)
 
@@ -56,3 +33,26 @@ CATEGORY_CHOICES = (
 class Category(models.Model):
     name = models.CharField(max_length=30)
     category_choices = models.CharField(max_length=1, choices=CATEGORY_CHOICES)
+
+
+# Image model to display image details
+
+
+class Images(models.Model):
+    image_name = models.CharField(max_length=20)
+    image_description = models.CharField(max_length=30)
+    # post will contain the image content
+    post = models.TextField()
+    # one location may describe many images while many images may have one
+    # location
+    location = models.ForeignKey(Location)
+    category = models.ForeignKey(Category)
+
+    # __str__ will return string representation of the image model
+    # __str__ will enable us view our returned queries
+
+    def __str__(self):
+        return self.image_name
+
+    class Meta:
+        ordering = ['name']
