7ad0f4cbbe2c141525cd749584a37755ca13cad7
MrazTevin <millatevin@gmail.com>
2018-02-25 21:04:27 +0300

    category for models with its tuples

--- exhibit/display/models.py
+++ exhibit/display/models.py
@@ -34,4 +34,25 @@ class Location(models.Model):
         return self.name
 
 
+# iterable tuple to use as choices of category field
+CATEGORY_CHOICES = (
+    ('1', 'Vegetation'),
+    ('2', 'Trees'),
+    ('3', 'Nature'),
+    ('4', 'Abstract'),
+    ('5', 'IT & Computers'),
+    ('6', 'Animals'),
+    ('7', 'Illustrations'),
+    ('8', 'Arts'),
+    ('9', 'Business'),
+    ('10', 'Industries'),
+    ('11', 'Objects'),
+    ('12', 'Technology'),
+    ('13', 'People'),
+    ('14', 'Travel'),
+)
+
+
 class Category(models.Model):
+    name = models.CharField(max_length=30)
+    category_choices = models.CharField(max_length=1, choices=CATEGORY_CHOICES)
