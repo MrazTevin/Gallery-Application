a150c82a80202f50f07a80b9bb1e12cc77d65a19
MrazTevin <millatevin@gmail.com>
2018-02-26 15:00:27 +0300

    imported settings

--- exhibit/display/urls.py
+++ exhibit/display/urls.py
@@ -1,9 +1,13 @@
 from django.conf.urls import url
 from . import views
-
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     url(r'^$', views.welcome, name='welcome'),
     url(r'^home/$', views.home, name='home'),
     url(r'^search/$', views.search_results, name='search_results')
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
