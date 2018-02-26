a5e7cc07f4429351aca8f99cf1f60bb843721cf8
MrazTevin <millatevin@gmail.com>
2018-02-26 13:05:27 +0300

    view function for search_results

--- exhibit/display/views.py
+++ exhibit/display/views.py
@@ -16,3 +16,21 @@ def home(request):
     '''
 
     return render(request, 'home.html')
+
+
+def search_results(request):
+    '''
+    search function to display search search_results
+    args:
+    order defines category
+    '''
+    if 'order' in request.GET and request.GET["photo"]:
+        search_term = request.GET.get("photo")
+        searched_photos = Photo.search_by_order(search_term)
+        message = f"{seach_term}"
+
+        return render(request, 'search.html', {"message": message, "photos": searched_photos}')
+
+    else:
+        message = "You haven't searched for any term"
+        return_render(request, 'search.html', {"message": message})
