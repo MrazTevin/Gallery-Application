3a12861f37b5a0239d5cf2e599f0046f25b077e4
MrazTevin <millatevin@gmail.com>
2018-02-26 04:04:50 +0300

    first test for our places model

--- exhibit/display/tests.py
+++ exhibit/display/tests.py
@@ -2,3 +2,15 @@ from django.test import TestCase
 from .models import Places, Order, Photos
 
 # Create your tests here.
+
+# setup method
+
+
+class PlacesTestClass(TestCase):
+    def setUp(self):
+        self.outdoor = Places(name='outdoor')
+
+
+# Testing instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.outdoor, Places))
