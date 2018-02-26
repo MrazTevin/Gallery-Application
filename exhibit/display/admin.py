f60e4b6e06c56e9cd4dfff93cf8c6f113cc14049
MrazTevin <millatevin@gmail.com>
2018-02-26 09:08:45 +0300

    registered my models

--- exhibit/display/admin.py
+++ exhibit/display/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Places, Photos, Order
 
 # Register your models here.
+admin.site.register(Places)
+admin.site.register(Photos)
+admin.site.register(Order)
