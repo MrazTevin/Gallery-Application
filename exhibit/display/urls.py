c515b3bf9b26e5db73b0ae7c014d993ab61c5c58
MrazTevin <millatevin@gmail.com>
2018-02-26 14:10:49 +0300

    added s to results

--- exhibit/display/urls.py
+++ exhibit/display/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.welcome, name='welcome'),
     url(r'^home/$', views.home, name='home'),
-    url(r'^search/$', views.search_reults, name='search_results')
+    url(r'^search/$', views.search_results, name='search_results')
 ]
