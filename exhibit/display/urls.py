bcf25f98e1a28da922b61955d1b639b227364ad5
MrazTevin <millatevin@gmail.com>
2018-02-28 14:53:01 +0300

    removed the image id url pattern , added silicon valley url pattern

--- exhibit/display/urls.py
+++ exhibit/display/urls.py
@@ -7,7 +7,8 @@ urlpatterns = [
     url(r'^$', views.images, name='images'),
     url(r'^home/$', views.home, name='home'),
     url(r'^search/$', views.search_results, name='search_results'),
-    url(r'^image/(\d+)', views.image, name='image')
+    url(r'^image/(\d+)', views.image, name='image'),
+    url(r'^Silicon_Valley/', views.Silicon_Valley, name='Silicon_Valley')
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
